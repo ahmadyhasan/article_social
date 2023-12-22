1062c2338bc485e91d27a7616a2ede5af84aa550
ahmadyhasan <1988.ha.ah@gmail.com>
2023-12-22 19:14:06 +0330

    add pagination in setting

--- main_app/settings.py
+++ main_app/settings.py
@@ -142,3 +142,9 @@ SPECTACULAR_SETTINGS = {
 }
 # Default django user model
 AUTH_USER_MODEL = 'user.User'
+
+# set pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
