87e8ae3c038e3ea930d391a77c03b86fdee55052
ahmadyhasan <1988.ha.ah@gmail.com>
2023-12-22 16:54:59 +0330

    add user view and registration view

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.utils.translation import gettext as _
+from drf_spectacular.utils import extend_schema
+from rest_framework import mixins
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from apps.user.serializers import RegistrationSerializer, UserSerializer
+
+User = get_user_model()
+
+
+class UserView(mixins.ListModelMixin, GenericViewSet):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(is_active=True)
+
+
+class RegistrationView(APIView):
+    @extend_schema(request=RegistrationSerializer)
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        with transaction.atomic():
+            email = serializer.validated_data['email']
+
+            user = User.objects.create(
+                username=serializer.validated_data['username'],
+                first_name=serializer.validated_data['first_name'],
+                last_name=serializer.validated_data['last_name'],
+                email=email,
+                is_active=True,
+            )
+            user.set_password(serializer.validated_data['password'])
+
+        return Response(_('Create an user'))
