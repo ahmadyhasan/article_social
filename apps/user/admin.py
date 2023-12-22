dab25e9cf9fad31693c48f6d7c696450400047e8
ahmadyhasan <1988.ha.ah@gmail.com>
2023-12-22 16:53:22 +0330

    creat user model

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+User = get_user_model()
+
+
+@register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('is_active',)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from helpers.models import AbstractIdOnlyModel
+
+
+class User(AbstractIdOnlyModel, AbstractUser):
+    first_name = models.CharField(_('First Name'), max_length=50)
+    last_name = models.CharField(_('Last Name'), max_length=50)
+    email = models.EmailField(_('Email Address'), unique=True)
+    is_active = models.BooleanField(
+        _('Is Active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'
+        ),
+        db_index=True,
+    )
