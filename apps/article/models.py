3ebbdd4a5819fd8a415c0a72768fc43e5d28b2e7
ahmadyhasan <1988.ha.ah@gmail.com>
2023-12-24 23:49:27 +0330

    create article model

--- apps/article/models.py
+++ apps/article/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from apps.user.models import User
+from helpers.models import AbstractBaseModel
 
-# Create your models here.
+
+class Article(AbstractBaseModel):
+    author = models.ManyToManyField(User, verbose_name=_('Author'), related_name='articles', db_index=True)
+    title = models.CharField(_('Title'), max_length=300, db_index=True)
+    content = models.TextField(_('Content'))
+    is_publish = models.BooleanField(_('Is Publish'), default=True)
+
+    def __str__(self):
+        return self.title
