e0a0e63385d9a6b48e97f1ee8b332d5bbff8f037
ahmadyhasan <1988.ha.ah@gmail.com>
2023-12-26 22:01:21 +0330

    add article view

--- apps/article/views.py
+++ apps/article/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins, filters
+from apps.article.serializers import RegistrationSerializer, ArticleSerializer
 
-class ArticleView():
-    pass
+from apps.article.models import Article
 
+
+class ArticleView(mixins.ListModelMixin, GenericViewSet):
+    serializer_class = ArticleSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['author', 'title']
+
+    def get_queryset(self):
+        return Article.objects.filter(is_publish=True)
