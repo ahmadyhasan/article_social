e1cd0cf5534713308d1950fbe262409e5d24fb48
ahmadyhasan <1988.ha.ah@gmail.com>
2023-12-27 21:41:02 +0330

    add article serializer

--- apps/article/views.py
+++ apps/article/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins, filters
-from apps.article.serializers import RegistrationSerializer, ArticleSerializer
+from apps.article.serializers import ArticleSerializer
 
 from apps.article.models import Article
 
