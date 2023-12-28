2962a85e6884251df86dd60cc216c079371c20af
ahmadyhasan <1988.ha.ah@gmail.com>
2023-12-28 22:09:15 +0330

    add CreateModelMixin

--- apps/article/views.py
+++ apps/article/views.py
@@ -5,7 +5,7 @@ from apps.article.serializers import ArticleSerializer
 from apps.article.models import Article
 
 
-class ArticleView(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
+class ArticleView(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin,GenericViewSet):
     serializer_class = ArticleSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['author', 'title']
