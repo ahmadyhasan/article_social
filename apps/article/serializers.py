d82699fcff2bb3629a250390f74c5397b1a27611
ahmadyhasan <1988.ha.ah@gmail.com>
2024-01-14 20:17:52 +0330

    change write to read

--- apps/article/serializers.py
+++ apps/article/serializers.py
@@ -6,4 +6,4 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('id', 'author', 'title', 'content', 'create_time', 'update_time', 'is_publish')
-        extra_kwargs = {'is_publish': {'write_only': True}}
+        extra_kwargs = {'is_publish': {'read_only': True}}
