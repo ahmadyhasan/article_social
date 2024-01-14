6799b95c2b45456408de55cdf50d4258e129b332
ahmadyhasan <1988.ha.ah@gmail.com>
2024-01-14 20:20:20 +0330

    move def create from view to serializer

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,18 +1,12 @@
 from django.contrib.auth import get_user_model
-from django.db import transaction
-from django.utils.translation import gettext as _
-from drf_spectacular.utils import extend_schema
 from rest_framework import mixins, filters
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
-
 from apps.user.serializers import RegistrationSerializer, UserSerializer
 
 User = get_user_model()
 
 
-class UserView(mixins.ListModelMixin, GenericViewSet):
+class UserView(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet):
     serializer_class = UserSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['username', 'email']
@@ -21,23 +15,5 @@ class UserView(mixins.ListModelMixin, GenericViewSet):
         return User.objects.filter(is_active=True)
 
 
-class RegistrationView(APIView):
-    @extend_schema(request=RegistrationSerializer)
-    def post(self, request):
-        serializer = RegistrationSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        with transaction.atomic():
-            email = serializer.validated_data['email']
-
-            user = User.objects.create(
-                username=serializer.validated_data['username'],
-                first_name=serializer.validated_data['first_name'],
-                last_name=serializer.validated_data['last_name'],
-                email=email,
-                is_active=True,
-            )
-            user.set_password(serializer.validated_data['password'])
-            user.save()
-
-        return Response(_('Create an user'))
+class RegistrationView(mixins.CreateModelMixin, GenericViewSet):
+    serializer_class = RegistrationSerializer
