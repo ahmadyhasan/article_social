2a0e9930035374613dc959c2c84a4350bcc1858e
ahmadyhasan <1988.ha.ah@gmail.com>
2024-02-09 23:23:51 +0330

    Create category model

--- apps/category/models.py
+++ apps/category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from helpers.models import AbstractBaseModel
+
+
+class Category(AbstractBaseModel):
+    name = models.CharField(_('Name'), max_length=100, db_index=True, unique=True)
+
+    def __str__(self):
+        return self.name
