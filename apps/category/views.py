ae41982cabe850355cf0685568597079d82d3b18
ahmadyhasan <1988.ha.ah@gmail.com>
2024-02-09 23:42:23 +0330

    create category views

--- apps/category/views.py
+++ apps/category/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
+from apps.category.serializers import CategorySerializer
 
-# Create your views here.
+from apps.category.models import Category
+
+
+class CategoryView(mixins.ListModelMixin, GenericViewSet,):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        return Category.objects.filter(all)
