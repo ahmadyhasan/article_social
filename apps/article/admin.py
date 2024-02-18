fc98fde22b8ae69569498f79c2a17bf79987770d
ahmadyhasan <1988.ha.ah@gmail.com>
2024-02-18 22:10:23 +0330

    add tag field in article views

--- apps/article/admin.py
+++ apps/article/admin.py
@@ -5,4 +5,4 @@ from apps.article.models import Article
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'get_authors', 'is_publish', 'get_categories')
+    list_display = ('title', 'get_authors', 'is_publish', 'get_categories', 'get_tags')
