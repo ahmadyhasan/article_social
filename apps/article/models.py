bd62750afacc51a43e93fc35db4f645dd81522c8
ahmadyhasan <1988.ha.ah@gmail.com>
2024-02-18 22:09:46 +0330

    add slug field in article model

--- apps/article/models.py
+++ apps/article/models.py
@@ -4,10 +4,12 @@ from django.utils.translation import gettext_lazy as _
 
 from apps.user.models import User
 from apps.category.models import Category
+from apps.tag.models import Tag
 from helpers.models import AbstractBaseModel
 
 
 class Article(AbstractBaseModel):
+    tags = models.ManyToManyField(Tag, verbose_name=_('Tags'), related_name='articles', db_index=True)
     category = models.ManyToManyField(Category, verbose_name=_('Category'), related_name='articles', db_index=True)
     author = models.ManyToManyField(User, verbose_name=_('Author'), related_name='articles', db_index=True)
     title = models.CharField(_('Title'), max_length=300, db_index=True)
@@ -24,3 +26,7 @@ class Article(AbstractBaseModel):
     @admin.display(description='categories')
     def get_categories(self):
         return [category.name for category in self.category.all()]
+
+    @admin.display(description='tags')
+    def get_tags(self):
+        return [tags.slug for tags in self.tags.all()]
